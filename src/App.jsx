9d586f18c81279a3f2d9b85b726fbd31c4225ce9
Daniel José Martins Junior <daniel.jr2501@gmail.com>
2026-05-08 14:17:24 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+export default function App() {
+  return (
+    <main className="bg-[#fdf8f5] min-h-screen flex items-center justify-center">
+
+      <h1 className="text-5xl md:text-7xl font-['Playfair_Display'] text-[#1f1f1f]">
+        Espaço Glamour ✨
+      </h1>
+
+    </main>
+  )
+}
